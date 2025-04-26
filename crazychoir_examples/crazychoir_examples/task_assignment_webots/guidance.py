aa838b5e76a499ab5c9b834295ce0e0c07f0ae3a
MoAls18 <malsaadi124@gmail.com>
2025-04-27 02:37:55 +0300

    documentation and cleanup

--- crazychoir_examples/crazychoir_examples/task_assignment_webots/guidance.py
+++ crazychoir_examples/crazychoir_examples/task_assignment_webots/guidance.py
@@ -1,17 +1,17 @@
 import rclpy
-from choirbot.guidance.task import TaskGuidance, PositionTaskExecutor
-from choirbot.optimizer import TaskOptimizer
+from choirbot.guidance.task import PositionTaskExecutor, CBBAGuidance
 from choirbot.optimizer.cbba_optimizer import CBBAOptimizer
-from crazychoir.guidance.cbba_guidance import CBBAGuidance
+
 def main():
     rclpy.init()
 
     # initialize task guidance
-    opt_settings = {'max_iterations': 100, 'convergence_threshold': 3, 'task_value_weight': 0.95}
+    opt_settings = {'max_iterations': 100,
+                    'convergence_threshold': 3, 'task_value_weight': 0.95}
     executor = PositionTaskExecutor()
     optimizer = CBBAOptimizer(settings=opt_settings)
-    
-    guidance = TaskGuidance(optimizer, executor, None, 'pubsub', 'odom')
+
+    guidance = CBBAGuidance(optimizer, executor, None, 'pubsub', 'odom')
 
     rclpy.spin(guidance)
     rclpy.shutdown()
